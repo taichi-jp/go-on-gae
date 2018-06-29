609bd7800601327f9ea217b7ca071d58a5c9c124
taichikoumoto <sbskomoto@gmail.com>
2018-06-30 01:46:13 +0900

    feat(POST, template): Handling HTML Form Data

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 
 	"google.golang.org/appengine"
 	// Required external App Engine library
 )
+
+var (
+	indexTemplate = template.Must(template.ParseFiles("index.html"))
+)
+
+type templateParams struct {
+	Notice string
+	Name   string
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	appengine.Main()
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	// if statement redirects all invalid URLs to the root homepage.
@@ -15,9 +31,32 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusFound)
 		return
 	}
-}
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	appengine.Main()
+	params := templateParams{}
+
+	if r.Method == "GET" {
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// It's a POST request, so handle the form submission.
+
+	name := r.FormValue("name")
+	params.Name = name
+	if name == "" {
+		name = "Anonymous Gopher"
+	}
+
+	if r.FormValue("message") == "" {
+		w.WriteHeader(http.StatusBadRequest)
+
+		params.Notice = "No message provided"
+		indexTemplate.Execute(w, params)
+		return
+	}
+
+	// TODO: save the message into a database.
+
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", name)
+	indexTemplate.Execute(w, params)
 }
